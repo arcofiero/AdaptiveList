afde836e4a63a44a6eb685c7d0c864d29b82b76d
Archit <araj2210@gmail.com>
2019-01-09 00:00:31 +0530

    Adaptive List

---
+++ testProj3.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.iastate.cs228.proj3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Archit Raj
+ *
+ */
+class testProj3 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
